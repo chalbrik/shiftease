3030bab7c266626876e93bcb0db826106e8d06bf
chalbrik <64196949+chalbrik@users.noreply.github.com>
2024-07-16 20:16:41 +0200

    Main beginning elements added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, LogoComponent, ScheduleComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Shiftease_1';
+  title = 'Shiftease';
 }
